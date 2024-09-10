f65f96384c439bfb16c0abb64e465048a5e52ce1
romintomasetti <romin.tomasetti@gmail.com>
2024-09-10 20:10:56 +0000

    graph: add intertwining case

---
+++ tests/graph/intertwine/test_kokkos.cpp
@@ -0,0 +1,86 @@
+#include "gtest/gtest.h"
+
+#include "Kokkos_Core.hpp"
+#include "Kokkos_Graph.hpp"
+
+#include "tests/graph/diamond/Helpers.hpp"
+
+/**
+ * @addtogroup unittests
+ *
+ * Intertwining with @c Kokkos
+ * ---------------------------
+ *
+ * Create an intertwining graph with @c Kokkos, inspired by the diamond case (see @ref diamond/test_kokkos.cpp).
+ *
+ * The test can be found in @ref intertwine/test_kokkos.cpp.
+ */
+
+namespace tests::graph::intertwine
+{
+
+/**
+ * @brief Faking an external library call that adds its workloads to the chain.
+ *
+ * It adds 2 asynchronous workloads. The input sender is split, and both workloads are
+ * joined and returned.
+ */
+template <typename Sender, typename ViewType>
+decltype(auto) library(const Sender& input, ViewType data)
+{
+    using policy_t = Kokkos::RangePolicy<typename ViewType::execution_space>;
+
+    auto one = input.then_parallel_for(
+        policy_t(0, data.size() / 2),
+        diamond::AddValueOffset{.data = data, .value = diamond::Values::value_B});
+
+    auto two = input.then_parallel_for(
+        policy_t(data.size() / 2, data.size()),
+        diamond::AddValueOffset{.data = std::move(data), .value = diamond::Values::value_C});
+
+    return Kokkos::Experimental::when_all(std::move(one), std::move(two));
+}
+
+//! @test Intertwine graph using @c Kokkos.
+TEST(graph, intertwine_kokkos)
+{
+    //! Use @c Kokkos::DefaultExecutionSpace because we can synchronize in this setup.
+    using execution_space = Kokkos::DefaultExecutionSpace;
+    using memory_space    = typename execution_space::memory_space;
+
+    //! Definition of the data type.
+    constexpr size_t size = 10;
+    static_assert(size % 2 == 0);
+
+    using view_t = Kokkos::View<int[size], memory_space>;
+
+    //! Get some execution context.
+    const execution_space exec {};
+
+    //! Initialize the data.
+    view_t data(Kokkos::view_alloc(exec, "data"));
+
+    //! Define the graph. Use a simple syntax.
+    using policy_t = Kokkos::RangePolicy<execution_space>;
+
+    auto graph = Kokkos::Experimental::create_graph<execution_space>(exec);
+
+    auto root = Kokkos::Impl::GraphAccess::create_root_ref(graph);
+
+    auto node_A = root.then_parallel_for(
+        policy_t(0, size),
+        diamond::AddValueOffset{.data = data, .value = diamond::Values::value_A});
+
+    auto continued = library(node_A, data);
+
+    auto node_D = continued.then_parallel_for(
+        policy_t(0, size),
+        diamond::AddValueOffset{.data = data, .value = diamond::Values::value_D});
+
+    //! Execute the graph and check results.
+    graph.submit(exec);
+
+    ASSERT_TRUE(diamond::check_data(exec, data));
+}
+
+} // namespace tests::graph::intertwine
---
+++ tests/graph/intertwine/test_stdexec.cpp
@@ -0,0 +1,90 @@
+#include "gtest/gtest.h"
+
+#include "tests/IgnoreWarnings.hpp"
+PRAGMA_DIAGNOSTIC_PUSH
+PRAGMA_DIAGNOSTIC_IGNORED("-Wunused-parameter")
+PRAGMA_DIAGNOSTIC_IGNORED("-Wdeprecated-copy")
+#include "exec/static_thread_pool.hpp"
+#include "stdexec/execution.hpp"
+PRAGMA_DIAGNOSTIC_POP
+
+#include "Kokkos_Core.hpp"
+
+#include "tests/graph/diamond/Helpers.hpp"
+
+/**
+ * @addtogroup unittests
+ *
+ * Intertwining with @c stdexec
+ * ----------------------------
+ *
+ * Create an intertwining graph with @c stdexec, inspired by the diamond case (see @ref diamond/test_stdexec.cpp).
+ *
+ * The test can be found in @ref intertwine/test_stdexec.cpp.
+ */
+
+namespace tests::graph::intertwine
+{
+
+/**
+ * @brief Faking an external library call that adds its workloads to the chain.
+ *
+ * It adds 2 asynchronous workloads. The input sender is split, and both workloads are
+ * joined and returned.
+ */
+template <typename Sender, typename ViewType>
+decltype(auto) library(Sender&& input, ViewType data)
+{
+    auto continued = std::forward<Sender>(input) | ::stdexec::split();
+
+    ::stdexec::sender auto one = continued | ::stdexec::bulk(
+        data.size() / 2,
+        diamond::AddValueOffset{.data = data, .value = diamond::Values::value_B});
+
+    ::stdexec::sender auto two = continued | ::stdexec::bulk(
+        data.size() / 2,
+        diamond::AddValueOffset{.data = std::move(data), .value = diamond::Values::value_C, .offset = data.size() / 2});
+
+    return ::stdexec::when_all(std::move(one), std::move(two));
+}
+
+//! @test Intertwine graph using @c stdexec.
+TEST(graph, intertwine_stdexec)
+{
+    //! Use @c Kokkos::Serial because we have no mean to synchronize in this setup.
+    using execution_space = Kokkos::Serial;
+    using memory_space    = typename execution_space::memory_space;
+
+    //! Definition of the data type.
+    constexpr size_t size = 10;
+    static_assert(size % 2 == 0);
+
+    using view_t = Kokkos::View<int[size], memory_space>;
+
+    //! Get some execution context.
+    ::exec::static_thread_pool pool{1};
+
+    //! Initialize the data.
+    view_t data("data");
+
+    //! Define the graph. Use a simple syntax.
+    ::stdexec::sender auto entry = ::stdexec::just();
+
+    ::stdexec::sender auto node_A = entry | ::stdexec::bulk(
+        size,
+        diamond::AddValueOffset{.data = data, .value = diamond::Values::value_A})
+        | ::stdexec::split();
+
+    ::stdexec::sender auto continued = library(node_A, data);
+
+    ::stdexec::sender auto node_D = continued | ::stdexec::bulk(
+        size,
+        diamond::AddValueOffset{.data = data, .value = diamond::Values::value_D});
+
+    //! Execute the graph and check results.
+    ::stdexec::sync_wait(::stdexec::start_on(pool.get_scheduler(), node_D));
+
+    ASSERT_TRUE(diamond::check_data(execution_space{}, data));
+}
+
+} // namespace tests::graph::intertwine
