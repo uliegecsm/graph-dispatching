ec2e18bcd772433f23f61e041bae8d4a48a1bbd5
romintomasetti <romin.tomasetti@gmail.com>
2024-10-11 20:42:44 +0000

    kokkos(ext): more files to help read all that stuff

---
+++ kokkos_ext/impl/PartialAlgorithm_ParallelFor.hpp
@@ -0,0 +1,44 @@
+#ifndef GRAPH_DISPATCHING_KOKKOS_EXT_IMPL_PARTIALALGORITHM_PARALLELFOR_HPP
+#define GRAPH_DISPATCHING_KOKKOS_EXT_IMPL_PARTIALALGORITHM_PARALLELFOR_HPP
+
+#include "kokkos_ext/impl/Concepts.hpp"
+#include "kokkos_ext/impl/PartialAlgorithm.hpp"
+
+namespace Kokkos::Experimental::graph::details
+{
+
+//! Specialization for @c Kokkos parallel-for.
+template <typename Policy, typename Functor>
+struct PartialAlgorithm<Kokkos::ParallelForTag, Policy, Functor>
+{
+    Policy policy;
+    Functor functor;
+
+    template <typename Sender> requires is_graph_sender<std::remove_reference_t<Sender>>
+    decltype(auto) operator()(Sender&& input)
+    {
+        return std::forward<Sender>(input).then_parallel_for(
+            std::move(policy),
+            std::move(functor)
+        );
+    }
+
+    /// Case of input sender being a @c Kokkos execution space instance.
+    /// We do eager execution, to get as close as possible from what a user currently
+    /// expects with vanilla @c Kokkos code.
+    template <typename Sender> requires (! is_graph_sender<std::remove_reference_t<Sender>>)
+    decltype(auto) operator()(Sender&& exec)
+    {
+        /// @todo The policy should be modified to ensure we run the kernel on the given
+        ///       execution space instance. This is currently not possible.
+        Kokkos::parallel_for(
+            std::move(policy),
+            std::move(functor)
+        );
+        return std::forward<Sender>(exec);
+    }
+};
+
+} // namespace Kokkos::Experimental::graph::details
+
+#endif // GRAPH_DISPATCHING_KOKKOS_EXT_IMPL_PARTIALALGORITHM_PARALLELFOR_HPP
