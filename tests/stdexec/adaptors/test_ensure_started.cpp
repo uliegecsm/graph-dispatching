8f5633209d4b973e1bbe1afdd17f14a536f98b7f
romintomasetti <romin.tomasetti@gmail.com>
2025-07-14 09:17:42 +0000

    tests(stdexec): `sync_wait` and `ensure_started`
    Signed-off-by: romintomasetti <romin.tomasetti@gmail.com>

---
+++ tests/stdexec/adaptors/test_ensure_started.cpp
@@ -0,0 +1,81 @@
+#include <chrono>
+#include <format>
+#include <iostream>
+#include <vector>
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+#include "tests/IgnoreWarnings.hpp"
+PRAGMA_DIAGNOSTIC_PUSH
+PRAGMA_DIAGNOSTIC_IGNORED("-Wunused-parameter")
+PRAGMA_DIAGNOSTIC_IGNORED("-Wdeprecated-copy")
+#include "stdexec/execution.hpp"
+PRAGMA_DIAGNOSTIC_POP
+
+#include "tests/Utils.hpp"
+#include "tests/stdexec/Utils.hpp"
+
+/**
+ * @addtogroup unittests
+ *
+ * Tests for @c stdexec::ensure_started
+ * ------------------------------------
+ *
+ * This group of tests check the behavior of @c stdexec::ensure_started.
+ *
+ * The test can be found in @ref tests/stdexec/adaptors/test_ensure_started.cpp.
+ *
+ * @note In the long term, @c stdexec::ensure_started might be dropped for @c exec::async_scope.
+ */
+
+namespace tests::stdexec::adaptors
+{
+
+class EnsureStartedTest : public utils::StaticThreadPool<'A'>, public ::testing::Test {};
+
+/// @test Simple @c stdexec::ensure_started test to check that the senders consumer starts to asynchronously consume the chain.
+TEST_F(EnsureStartedTest, asynchronously_consumes)
+{
+    using duration_t = std::chrono::steady_clock::duration;
+
+    const auto start = std::chrono::steady_clock::now();
+
+    ::stdexec::sender auto chain_A = ::stdexec::schedule(std::get<0>(this->pools).get_scheduler())
+        | ::stdexec::then([&start] () -> duration_t {
+            return std::chrono::steady_clock::now() - start;
+        });
+
+    //! Ensure that we start consuming.
+    auto handle_A = ::stdexec::ensure_started(chain_A);
+
+    const auto after_ensure_started = std::chrono::steady_clock::now();
+
+    //! We continue adding stuff.
+    ::stdexec::sender auto chain_B = std::move(chain_A) // NOLINT(performance-move-const-arg)
+        | ::stdexec::then([&start] (const auto&) {
+            return std::chrono::steady_clock::now() - start;
+        });
+
+    //! But before we wait for the whole chain, let's explicitly wait for the first part.
+    const auto [value_A] = ::stdexec::sync_wait(std::move(handle_A)).value();
+
+    const auto after_sync_wait_A = std::chrono::steady_clock::now();
+
+    const auto [value_B] = ::stdexec::sync_wait(std::move(chain_B)).value(); // NOLINT(performance-move-const-arg)
+
+    const auto end = std::chrono::steady_clock::now();
+
+    //! The work in the first part cannot have started before the call to @c stdexec::ensure_started.
+    ASSERT_LT((after_ensure_started - start).count(), value_A.count());
+
+    //! The work in the first part is finished once we've synchronized its handle.
+    ASSERT_LT(value_A.count(), (after_sync_wait_A - start).count());
+
+    //! Then only comes the work of the second part.
+    ASSERT_LT((after_sync_wait_A - start).count(), value_B.count());
+
+    ASSERT_LT(value_B.count(), (end - start).count());
+}
+
+} // namespace tests::stdexec::adaptors
---
+++ tests/stdexec/adaptors/test_sync_wait.cpp
@@ -0,0 +1,49 @@
+#include <vector>
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+#include "tests/IgnoreWarnings.hpp"
+PRAGMA_DIAGNOSTIC_PUSH
+PRAGMA_DIAGNOSTIC_IGNORED("-Wunused-parameter")
+PRAGMA_DIAGNOSTIC_IGNORED("-Wdeprecated-copy")
+#include "stdexec/execution.hpp"
+PRAGMA_DIAGNOSTIC_POP
+
+#include "tests/Utils.hpp"
+#include "tests/stdexec/Utils.hpp"
+
+/**
+ * @addtogroup unittests
+ *
+ * Tests for @c stdexec::sync_wait
+ * -------------------------------
+ *
+ * This group of tests check the behavior of @c stdexec::sync_wait.
+ *
+ * The test can be found in @ref tests/stdexec/adaptors/test_sync_wait.cpp.
+ */
+
+namespace tests::stdexec::adaptors
+{
+
+class SyncWaitTest : public utils::StaticThreadPool<'A'>, public ::testing::Test {};
+
+/// @test Simple @c stdexec::sync_wait test that blocks the current thread until the result is known.
+TEST_F(SyncWaitTest, block)
+{
+    ::stdexec::sender auto chain = ::stdexec::schedule(std::get<0>(this->pools).get_scheduler())
+        | ::stdexec::then([this] () -> double {
+            if(std::this_thread::get_id() == this->main)
+                throw std::runtime_error("Unexpected.");
+            return 42.;
+        });
+
+    const auto [result] = ::stdexec::sync_wait(std::move(chain)).value(); // NOLINT(performance-move-const-arg)
+
+    static_assert(std::same_as<decltype(result), const double>);
+
+    ASSERT_EQ(result, 42.);
+}
+
+} // namespace tests::stdexec::adaptors
