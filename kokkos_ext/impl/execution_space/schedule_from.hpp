f7bc63e6208abfcff78284f5832c253289634d95
romintomasetti <romin.tomasetti@gmail.com>
2026-01-08 19:42:16 +0000

    kokkos(continues_on): transition off of
    Signed-off-by: romintomasetti <romin.tomasetti@gmail.com>

---
+++ kokkos_ext/impl/execution_space/schedule_from.hpp
@@ -0,0 +1,76 @@
+#ifndef GRAPH_DISPATCHING_KOKKOS_EXT_IMPL_EXECUTION_SPACE_SCHEDULE_FROM_HPP
+#define GRAPH_DISPATCHING_KOKKOS_EXT_IMPL_EXECUTION_SPACE_SCHEDULE_FROM_HPP
+
+#include "stdexec/execution.hpp"
+
+#include "kokkos_ext/impl/ExecutionSpaceContext_fwd.hpp"
+
+namespace Kokkos::Experimental::details::execution_space
+{
+
+//! Receiver for @c schedule_from.
+template <typename Env, stdexec::receiver Rcvr>
+struct ScheduleFromReceiver
+{
+    using receiver_concept = stdexec::receiver_t;
+
+    Env env;
+    Rcvr rcvr;
+    bool skip;
+
+    void set_value() && noexcept {
+        if (!skip) env.exec.fence(std::format("{}: schedule_from", Kokkos::Impl::TypeInfo<decltype(env.exec)>::name()));
+        ::stdexec::set_value(std::move(rcvr));
+    }
+
+    template <class Error>
+    void set_error(Error&& err) && noexcept {
+        ::stdexec::set_error(std::move(rcvr), std::forward<Error>(err));
+    }
+
+    decltype(auto) get_env() const noexcept { return env; }
+};
+
+//! Sender for @c schedule_from.
+template <typename Env, ::stdexec::sender Sndr>
+struct ScheduleFromSender
+{
+    using sender_concept = ::stdexec::sender_t;
+
+    //! The transition may throw or forwards the error channel.
+    using with_error_invoke_t = ::stdexec::completion_signatures<
+        ::stdexec::set_value_t(),
+        ::stdexec::set_error_t(std::exception_ptr)
+    >;
+
+    template <typename Self, typename... Env_>
+    using completion_signatures = ::stdexec::transform_completion_signatures<
+        ::stdexec::completion_signatures_of_t<::stdexec::__copy_cvref_t<Self, Sndr>, Env_...>,
+        with_error_invoke_t
+    >;
+
+    //! As required by https://github.com/NVIDIA/stdexec/blob/3363435259b7ffae43d3f2e5f6b7a7b36d7cd7d3/include/stdexec/__detail/__diagnostics.hpp#L266-L310.
+    template <typename... Env_>
+    auto get_completion_signatures(Env_&&...) -> completion_signatures<ScheduleFromSender, Env_...> { return {}; } // NOLINT(cppcoreguidelines-missing-std-forward)
+
+    template <::stdexec::receiver Rcvr>
+    ::stdexec::operation_state auto connect(Rcvr&& rcvr) && noexcept(std::is_nothrow_move_constructible_v<Rcvr>)
+    {
+        using recv_t = ScheduleFromReceiver<Env, std::remove_cvref_t<Rcvr>>;
+
+        return ::stdexec::connect(
+            std::move(sndr),
+            recv_t{.env = std::move(env), .rcvr = std::forward<Rcvr>(rcvr), .skip = skip}
+        );
+    }
+
+    decltype(auto) get_env() const noexcept { return env; }
+
+    Env env;
+    Sndr sndr;
+    bool skip;
+};
+
+} // namespace Kokkos::Experimental::details::execution_space
+
+#endif // GRAPH_DISPATCHING_KOKKOS_EXT_IMPL_EXECUTION_SPACE_SCHEDULE_FROM_HPP
