ee9c8c0d9c9301e5d06321243bc352079c158c7c
Maarten Arnst <maarten.arnst@uliege.be>
2026-01-15 16:14:13 +0100

    kokkos(ext): add `get_exec_t` query
    Signed-off-by: Maarten Arnst <maarten.arnst@uliege.be>
    Signed-off-by: romintomasetti <romin.tomasetti@gmail.com>

--- sync_wait.hpp
+++ sync_wait.hpp
@@ -4,37 +4,38 @@
 #include "stdexec/execution.hpp"
 
 #include "kokkos_ext/impl/ExecutionSpaceContext_fwd.hpp"
+#include "kokkos_ext/impl/execution_space/get_exec.hpp"
 #include "kokkos_ext/impl/sync_wait.hpp"
 
 namespace Kokkos::Experimental::details::execution_space {
 
 //! Receiver for @c sync_wait.
-template <stdexec::__is_instance_of<Scheduler> Schd>
+template <Kokkos::ExecutionSpace Exec>
 struct SyncWaitReceiver {
     using receiver_concept = stdexec::receiver_t;
 
-    Schd schd;
+    Exec exec;
     Kokkos::Experimental::details::impl::State* state;
 
     void set_value() && noexcept {
-        schd.exec.fence(std::format("{}: sync_wait", Kokkos::Impl::TypeInfo<decltype(schd.exec)>::name()));
+        exec.fence(std::format("{}: sync_wait", Kokkos::Impl::TypeInfo<Exec>::name()));
         state->loop.finish();
     }
 
     template <typename Error>
     void set_error(Error&& err) && noexcept {
         state->error = std::forward<Error>(err);
-        schd.exec.fence(std::format("{}: sync_wait", Kokkos::Impl::TypeInfo<decltype(schd.exec)>::name()));
+        exec.fence(std::format("{}: sync_wait", Kokkos::Impl::TypeInfo<Exec>::name()));
         state->loop.finish();
     }
 
     void set_stopped() noexcept {
-        schd.exec.fence(std::format("{}: sync_wait", Kokkos::Impl::TypeInfo<decltype(schd.exec)>::name()));
+        exec.fence(std::format("{}: sync_wait", Kokkos::Impl::TypeInfo<Exec>::name()));
         state->loop.finish();
     }
 
     [[nodiscard]]
-    auto get_env() const noexcept -> Kokkos::Experimental::details::impl::env {
+    constexpr auto get_env() const noexcept -> Kokkos::Experimental::details::impl::env {
         return {state->loop.get_scheduler()};
     }
 };
@@ -46,12 +47,14 @@ struct SyncWait {
      *
      * @todo Make the @c noexcept specifier depend on the completion signatures of @p sndr.
      */
-    template <stdexec::scheduler Schd, stdexec::sender Sndr>
-    auto operator()(Schd&& schd, Sndr&& sndr) const noexcept(false) -> std::optional<std::tuple<>> {
+    template <stdexec::sender Sndr>
+    auto operator()(Sndr&& sndr) const noexcept(false) -> std::optional<std::tuple<>> {
         Kokkos::Experimental::details::impl::State state;
 
-        auto op_state = stdexec::connect(
-            std::forward<Sndr>(sndr), SyncWaitReceiver{.schd = std::forward<Schd>(schd), .state = &state});
+        auto schd = stdexec::get_completion_scheduler<stdexec::set_value_t>(stdexec::get_env(sndr));
+
+        auto op_state =
+            stdexec::connect(std::forward<Sndr>(sndr), SyncWaitReceiver{.exec = std::move(schd.exec), .state = &state});
 
         stdexec::start(op_state);
 
@@ -78,8 +81,7 @@ template <>
 struct apply_sender_for<stdexec::sync_wait_t> {
     template <execution_space_completing_sender Sndr>
     auto operator()(Sndr&& sndr) && noexcept(false) {
-        auto schd = stdexec::get_completion_scheduler<stdexec::set_value_t>(stdexec::get_env(sndr), stdexec::env{});
-        return SyncWait{}(std::move(schd), std::forward<Sndr>(sndr));
+        return SyncWait{}(std::forward<Sndr>(sndr));
     }
 };
 
