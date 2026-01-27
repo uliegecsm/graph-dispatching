40482b69f11c16ac48b7c5ec137d565ff2ecb6d4
romintomasetti <romin.tomasetti@gmail.com>
2026-01-27 12:12:45 +0000

    kokkos(ext): when_all
    Signed-off-by: romintomasetti <romin.tomasetti@gmail.com>

---
+++ kokkos_ext/impl/graph/when_all.hpp
@@ -0,0 +1,160 @@
+#ifndef GRAPH_DISPATCHING_KOKKOS_EXT_IMPL_GRAPH_WHEN_ALL_HPP
+#define GRAPH_DISPATCHING_KOKKOS_EXT_IMPL_GRAPH_WHEN_ALL_HPP
+
+#include "stdexec/execution.hpp"
+
+#include "kokkos_ext/impl/GraphContext_fwd.hpp"
+#include "kokkos_ext/impl/completion_signatures.hpp"
+
+namespace Kokkos::Experimental::details::graph {
+
+/**
+ * @brief Operation state for a @c when_all whose children are all completing on @ref Kokkos::Experimental::details::graph::Domain.
+ *
+ * @note It is assumed they all belong to the same underlying graph.
+ */
+template <stdexec::receiver InnerRcvr, ::stdexec::sender... Sndrs>
+struct WhenAllOpState {
+    using operation_state_concept = stdexec::operation_state_t;
+
+    //! Receiver for a child of @c when_all.
+    struct WhenAllChildReceiver {
+        using receiver_concept = stdexec::receiver_t;
+
+        WhenAllOpState* parent;
+
+        void set_value() noexcept {
+            parent->child_completed();
+        }
+
+        template <class Error>
+        void set_error(Error&& err) noexcept {
+            parent->child_error(std::forward<Error>(err));
+        }
+
+        [[nodiscard]]
+        constexpr auto get_env() const noexcept -> stdexec::env_of_t<InnerRcvr> {
+            return parent->get_env();
+        }
+    };
+
+    using children_opstates_t = stdexec::__tuple<stdexec::connect_result_t<Sndrs, WhenAllChildReceiver>...>;
+
+    InnerRcvr inner_rcvr;
+    children_opstates_t children_opstates;
+    std::exception_ptr error = nullptr;
+
+    using node_t = decltype(stdexec::__apply(
+        [](auto&... ops) { return Kokkos::Experimental::when_all(*ops.get_node()...); },
+        std::declval<children_opstates_t&>()));
+
+    std::optional<node_t> node = std::nullopt;
+
+    WhenAllOpState(stdexec::__tuple<Sndrs...>&& sndrs_, InnerRcvr&& inner_rcvr_)
+        : inner_rcvr(std::move(inner_rcvr_))
+        , children_opstates(
+              stdexec::__apply(
+                  [this]<typename... Children>(Children&&... children) -> children_opstates_t {
+                      return children_opstates_t{
+                          stdexec::connect(std::forward<Children>(children), WhenAllChildReceiver{this})...};
+                  },
+                  std::move(sndrs_)))
+        , node(this->create_node()) {
+    }
+
+    WhenAllOpState(const WhenAllOpState&) = delete;
+    WhenAllOpState& operator=(const WhenAllOpState&) = delete;
+    WhenAllOpState(WhenAllOpState&&) = delete;
+    WhenAllOpState& operator=(WhenAllOpState&&) = delete;
+    ~WhenAllOpState() = default;
+
+    auto create_node() {
+        return stdexec::__apply(
+            [](auto&... ops) { return Kokkos::Experimental::when_all(*ops.get_node()...); }, children_opstates);
+    }
+
+    //! Children will start and most probably complete very quickly, before the corresponding graph nodes are done.
+    void start() & noexcept {
+        stdexec::__apply([](auto&... ops) -> void { (stdexec::start(ops), ...); }, children_opstates);
+
+        //! @todo Find a way to do this conditionally depending on what's next.
+        stdexec::__get<0>(children_opstates)
+            .schd.state_ptr->wait(
+                std::format(
+                    "{}: when_all",
+                    Kokkos::Impl::TypeInfo<decltype(stdexec::__get<0>(children_opstates)
+                                                        .schd.state_ptr->exec)>::name()));
+
+        if (error != nullptr) {
+            stdexec::set_error(std::move(inner_rcvr), std::move(error));
+        } else {
+            stdexec::set_value(std::move(inner_rcvr));
+        }
+    }
+
+    void child_completed() noexcept {
+    }
+
+    template <class Error>
+    void child_error(Error&& err) noexcept {
+        this->error = std::forward<Error>(err);
+    }
+
+    //! @todo Check if this is correct.
+    auto get_env() const noexcept -> stdexec::env_of_t<InnerRcvr> {
+        return stdexec::get_env(inner_rcvr);
+    }
+};
+
+/**
+ * @brief Sender for @c stdexec::when_all.
+ *
+ * @warning It only accepts input senders that complete on the @ref Kokkos::Experimental::details::graph::Domain domain,
+ *          because it adds a @c Kokkos::Experimental::when_all node to the underlying graph.
+ */
+template <::stdexec::sender... Sndrs>
+struct WhenAllSender {
+    using sender_concept = ::stdexec::sender_t;
+
+    struct attrs {
+        template <typename... Env>
+        [[nodiscard]]
+        constexpr auto
+            query(stdexec::get_completion_domain_t<stdexec::set_value_t>, const Env&...) const noexcept -> Domain {
+            return {};
+        }
+    };
+
+    //! @todo Make it much more robust as per https://github.com/NVIDIA/stdexec/blob/0f8338ab55b03d63f5fef111cefed7ab4002e78d/include/stdexec/__detail/__when_all.hpp#L341.
+    template <typename Self, typename... Env>
+    using _completion_signatures =
+        stdexec::completion_signatures<stdexec::set_value_t(), stdexec::set_error_t(std::exception_ptr)>;
+
+    GRAPH_DISPATCHING_KOKKOS_EXT_COMPLETION_SIGNATURES(WhenAllSender)
+
+    template <::stdexec::receiver Rcvr>
+    ::stdexec::operation_state auto connect(Rcvr&& rcvr) && noexcept(std::is_nothrow_move_constructible_v<Rcvr>) {
+        return WhenAllOpState<Rcvr, Sndrs...>(std::move(sndrs), std::forward<Rcvr>(rcvr));
+    }
+
+    constexpr auto get_env() const noexcept -> attrs {
+        return {};
+    }
+
+    stdexec::__tuple<Sndrs...> sndrs;
+};
+
+template <typename Env>
+struct transform_sender_for<stdexec::when_all_t, Env> {
+    template <::stdexec::sender... Sndrs>
+    requires(graph_completing_sender<Sndrs, Env> && ...)
+    auto operator()(stdexec::when_all_t, ::stdexec::__ignore, Sndrs&&... sndrs) && noexcept {
+        return WhenAllSender<Sndrs...>{.sndrs = {std::forward<Sndrs>(sndrs)...}};
+    }
+
+    const Env& env_; // NOLINT(cppcoreguidelines-avoid-const-or-ref-data-members)
+};
+
+} // namespace Kokkos::Experimental::details::graph
+
+#endif // GRAPH_DISPATCHING_KOKKOS_EXT_IMPL_GRAPH_WHEN_ALL_HPP
---
+++ tests/kokkos_ext/graph/test_when_all.cpp
@@ -0,0 +1,122 @@
+#include <vector>
+
+#include "kokkos-utils/callbacks/RecorderListener.hpp"
+#include "kokkos-utils/tests/scoped/callbacks/Manager.hpp"
+
+#include "tests/CallbackMatchers.hpp"
+#include "tests/kokkos_ext/Helpers.hpp"
+#include "tests/kokkos_ext/graph/Helpers.hpp"
+#include "tests/stdexec/Utils.hpp"
+
+/**
+ * @addtogroup unittests
+ *
+ * Customization of @c when_all by @c Kokkos::Experimental::GraphContext
+ * ---------------------------------------------------------------------
+ *
+ * This group of tests check that @ref Kokkos::Experimental::GraphContext properly customizes
+ * @c when_all.
+ *
+ * The tests can be found in @ref tests/kokkos_ext/graph/test_when_all.cpp.
+ */
+
+using execution_space = Kokkos::DefaultExecutionSpace;
+
+namespace tests::kokkos_ext {
+
+using namespace Kokkos::utils::callbacks;
+
+class WhenAllTest
+    : public impl::GraphContextTest<execution_space>
+    , public Kokkos::utils::tests::scoped::callbacks::Manager {
+   public:
+    using recorder_listener_t = RecorderListener<BeginFenceEvent, BeginParallelForEvent, ProfileEvent>;
+    using variant_t = std::variant<BeginFenceEvent, BeginParallelForEvent, ProfileEvent>;
+};
+
+//! Check basic traits of a @c when_all sender.
+template <::stdexec::sender T>
+constexpr bool check_traits(const T&) {
+    static_assert(std::same_as<
+                  decltype(::stdexec::get_completion_domain<::stdexec::set_value_t>(
+                      ::stdexec::get_env(std::declval<T>()), ::stdexec::env<>{})),
+                  Kokkos::Experimental::details::graph::Domain
+    >);
+
+    static_assert(!tests::stdexec::has_completion_scheduler_for<T, ::stdexec::set_value_t>);
+
+    return true;
+}
+
+//! @test Check that @ref Kokkos::Experimental::GraphContext does its duty well when used with a single-branch @c when_all.
+TEST_F(WhenAllTest, one_branch) {
+    const view_s_t data(Kokkos::view_alloc(exec, "data - shared space"));
+
+    const context_t grc{exec};
+
+    auto when_all = ::stdexec::when_all(::stdexec::schedule(grc.get_scheduler()) | ADD_THEN);
+
+    static_assert(check_traits(when_all));
+
+    std::vector<::testing::Matcher<variant_t>> matchers{
+        MATCHER_FOR_PROFILE_EVENT(dispatch_label(exec, "graph instantiate")),
+        MATCHER_FOR_PROFILE_EVENT(dispatch_label(exec, "graph submit")),
+        KOKKOS_DEFAULTED_GRAPH_SUBMIT_FENCE(exec)};
+
+    if constexpr (::tests::kokkos_ext::impl::is_graph_defaulted<execution_space>) {
+        if (execution_space{} != exec) {
+            matchers.push_back(KOKKOS_DEFAULTED_GRAPH_SINK_SYNC(exec));
+            matchers.push_back(KOKKOS_DEFAULTED_GRAPH_ENDOF_SINK_SYNC(execution_space{}));
+        }
+    }
+    matchers.push_back(MATCHER_FOR_BEGIN_FENCE(exec, dispatch_label(exec, "when_all")));
+
+    ASSERT_EQ(data(), 0) << "Eager execution is not allowed.";
+
+    ASSERT_THAT(
+        recorder_listener_t::record(
+            [when_all = std::move(when_all)]() mutable { ::stdexec::sync_wait(std::move(when_all)); }),
+        ::testing::ElementsAreArray(matchers));
+
+    ASSERT_EQ(data(), 1);
+}
+
+//! @test Check that @ref Kokkos::Experimental::GraphContext does its duty well when used with a three-branches @c when_all.
+TEST_F(WhenAllTest, three_branches) {
+    const view_sa_t data(Kokkos::view_alloc(exec, "data - shared space"));
+
+    const context_t grc{exec};
+
+    auto branch_a = ::stdexec::schedule(grc.get_scheduler()) | ADD_THEN;
+    auto branch_b = ::stdexec::schedule(grc.get_scheduler()) | ADD_THEN;
+    auto branch_c = ::stdexec::schedule(grc.get_scheduler()) | ADD_THEN;
+    auto when_all = ::stdexec::when_all(std::move(branch_a), std::move(branch_b), std::move(branch_c));
+
+    static_assert(check_traits(when_all));
+
+    std::vector<::testing::Matcher<variant_t>> matchers{
+        MATCHER_FOR_PROFILE_EVENT(dispatch_label(exec, "graph instantiate")),
+        MATCHER_FOR_PROFILE_EVENT(dispatch_label(exec, "graph submit")),
+        KOKKOS_DEFAULTED_GRAPH_SUBMIT_FENCE(exec)};
+
+    if constexpr (::tests::kokkos_ext::impl::is_graph_defaulted<execution_space>) {
+        if (execution_space{} != exec) {
+            matchers.push_back(KOKKOS_DEFAULTED_GRAPH_SINK_SYNC(exec));
+            matchers.push_back(KOKKOS_DEFAULTED_GRAPH_SINK_SYNC(exec));
+            matchers.push_back(KOKKOS_DEFAULTED_GRAPH_SINK_SYNC(exec));
+            matchers.push_back(KOKKOS_DEFAULTED_GRAPH_ENDOF_SINK_SYNC(execution_space{}));
+        }
+    }
+    matchers.push_back(MATCHER_FOR_BEGIN_FENCE(exec, dispatch_label(exec, "when_all")));
+
+    ASSERT_EQ(data(), 0) << "Eager execution is not allowed.";
+
+    ASSERT_THAT(
+        recorder_listener_t::record(
+            [when_all = std::move(when_all)]() mutable { ::stdexec::sync_wait(std::move(when_all)); }),
+        ::testing::ElementsAreArray(matchers));
+
+    ASSERT_EQ(data(), 3);
+}
+
+} // namespace tests::kokkos_ext
