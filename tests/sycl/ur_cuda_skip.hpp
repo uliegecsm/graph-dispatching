50bd0be827748921521f02837aceb4ad8ac93e7d
Tomasetti Romin <romin.tomasetti@gmail.com>
2026-06-15 17:38:44 +0200

    clang(format): ur cuda skip
    Signed-off-by: romintomasetti <romin.tomasetti@gmail.com>

--- tests/sycl/ur_cuda_skip.hpp
+++ tests/sycl/ur_cuda_skip.hpp
@@ -2,7 +2,7 @@
 #define GRAPH_DISPATCHING_TESTS_SYCL_UR_CUDA_SKIP_HPP
 
 #if defined(KOKKOS_ENABLE_SYCL)
-#include "sycl/version.hpp"
+#    include "sycl/version.hpp"
 #endif
 
 /**
@@ -13,15 +13,16 @@
  * See https://github.com/intel/llvm/issues/22322.
  */
 #if defined(KOKKOS_ENABLE_SYCL) && defined(KOKKOS_IMPL_ARCH_NVIDIA_GPU)
-    #if !defined(__SYCL_COMPILER_VERSION)
-        #error "SYCL compiler version is not defined."
-    #elif __SYCL_COMPILER_VERSION == 20260319
-        #define GRAPH_DISPATCHING_TESTS_SYCL_UR_CUDA_SKIP GTEST_SKIP() << "Known to fail on SYCL with Unified Runtime CUDA adaptor.";
-    #else
-        #define GRAPH_DISPATCHING_TESTS_SYCL_UR_CUDA_SKIP
-    #endif
+#    if !defined(__SYCL_COMPILER_VERSION)
+#        error "SYCL compiler version is not defined."
+#    elif __SYCL_COMPILER_VERSION == 20260319
+#        define GRAPH_DISPATCHING_TESTS_SYCL_UR_CUDA_SKIP                                                              \
+            GTEST_SKIP() << "Known to fail on SYCL with Unified Runtime CUDA adaptor.";
+#    else
+#        define GRAPH_DISPATCHING_TESTS_SYCL_UR_CUDA_SKIP
+#    endif
 #else
-    #define GRAPH_DISPATCHING_TESTS_SYCL_UR_CUDA_SKIP
+#    define GRAPH_DISPATCHING_TESTS_SYCL_UR_CUDA_SKIP
 #endif
 
 #endif // GRAPH_DISPATCHING_TESTS_SYCL_UR_CUDA_SKIP_HPP
